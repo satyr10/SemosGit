d4a0e55ebab733d2ebcbd2062f4ad0270bbd0eeb
satyr10 <58231214+satyr10@users.noreply.github.com>
2019-12-20 21:25:13 +0100

    Add files via upload

---
+++ JAVAScript/FirstExercise.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+var imePrezime = 'Ilija Mitrov'; //string
+var vozrast = 24; // number (integer)
+var visina = 182.4; // number (float)
+var vozachka_dozvola = false; // boolean
+
+console.log(imePrezime)
+console.log(vozrast)
+console.log(visina)
+console.log(vozachka_dozvola)
+var test = 'Ova e eden test'
+console.log('Ova e eden test ' + visina + ' ' + vozrast)
+
+var ime = 'Adrian';
+var prezime = 'Tochi';
+var vozrast = 30;
+var visina = 184.5;
+var tezhina = 85;
+var vraboten = true;
+var vozachka_dozvola = false;
+var zdravstveno_osiguruvanje = true;
+var adresa = 'Nikola Maleshevski';
+var kurs = 'SEMOS';
+
+console.log(ime)
+console.log(prezime)
+console.log(vozrast)
+console.log(visina)
+console.log(tezhina)
+console.log(vraboten)
+console.log(vozachka_dozvola)
+console.log(zdravstveno_osiguruvanje)
+console.log(adresa)
+console.log(kurs)
+
+console.log('Jas sum ' + ime + ' ' + prezime + ' ' + vozrast + ' godini od Skopje, visok ' + visina + ' i posetuvam kurs vo ' + kurs +'.')
+
+var str = 'Hello'
+var test = str[5];
+console.log(str[4]);
+
+
+
+
---
+++ JAVAScript/Vezba1.js
@@ -0,0 +1,47 @@
+
+
+
+
+// stringObj = new String('Hello')
+// console.log(stringObj.length)
+// console.log(stringObj.substring(2,4))
+// console.log(stringObj.charAt(0))
+
+// pi = Math.PI
+// console.log(pi)
+// e = Math.E
+// console.log(e)
+// sin = Math.sin(1.56)
+// console.log(sin)
+// log = Math.log(10)
+// console.log(log)
+// round = Math.round(10.10004020500)
+// console.log(round)
+
+const symbol1 = Symbol();
+const symbol2 = Symbol(42);
+const symbol3 = Symbol('foo');
+
+console.log(typeof symbol1);
+console.log(symbol3.toString());
+console.log(Symbol('foo') === Symbol('foo'));
+
+// var x = 2
+// var y = 39
+// var z = "42"
+// console.log(eval("x + y + 1"))
+// console.log(eval(z))
+
+// today = new Date()
+// birthday = new Date("December 17, 1995 03:24:00")
+// birthday = new Date(95,12,17)
+// birthday = new Date(95,12,17,3,24,0)
+// console.log(birthday)
+
+// var upperText="ALPHABET"
+// upperText.toLowerCase()
+// var lowerText ="alphabet"
+// lowerText.toUpperCase()
+
+// console.log(lowerText.toUpperCase)
+
