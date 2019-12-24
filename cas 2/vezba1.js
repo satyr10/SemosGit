a3430c8ead8e0d64523636e7f370ae4bad3fcab7
Adrian Tochi <58231214+satyr10@users.noreply.github.com>
2019-12-24 22:01:39 +0100

    Add files via upload

---
+++ cas 2/vezba1.js
@@ -0,0 +1,78 @@
+// var grade = 58;
+// var passed = false;
+
+// if (grade>=60){
+// 	console.log("Polozen!")}
+// 	else if (grade<50){
+// 		console.log("Ne polozen!")}
+// 		else if (passed == true){
+// 			console.log("Polozen!")}
+// else {
+// 	console.log("Vnesete povtorno poeni!")
+// }
+
+
+
+// var today = new Date();
+// console.log(today)
+// var hour = today.getHours();
+// console.log(hour)
+
+// if (hour == 15) {
+// 	console.log("Na vreme!")
+// }
+// else if (hour <= 20) {
+// 	console.log("Pred vreme!")
+// }
+// else if (hour > 20) {
+// 	console.log("Docna!")
+// }
+// else{
+// 	console.log("Vnesete tocno vreme!")
+// }
+
+
+var today = new Date()
+var todaynumber = today.getDay();
+console.log(todaynumber);
+switch (todaynumber){
+	case 0:
+	console.log("Sunday")
+	break;
+	case 1:
+	console.log("Monday")
+	break;
+	case 2:
+	console.log("Tuesday")
+	break;
+	case 3:
+	console.log("Wednesday")
+	break;
+	case 4:
+	console.log("Thursday")
+	break;
+	case 5:
+	console.log("Friday")
+	break;
+	case 6:
+	console.log("Saturday")
+	break;
+	default:
+	console.log("Greska")
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
