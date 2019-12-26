ff80cdce9325583f4735f335ab87ba29f2e0c6fb
Adrian Tochi <58231214+satyr10@users.noreply.github.com>
2019-12-26 21:54:32 +0100

    Add files via upload

---
+++ Cas 3/Vezba.js
@@ -0,0 +1,129 @@
+
+
+// var test = 5
+
+// if (test ==5){break;}
+// 	console.log("TUKA SUM");
+
+// var y = "5" + 5;
+// console.log(y);
+// console.log(typeof y);
+
+
+// var i;
+// for (i=0; i<10; i++){
+// 	console.log("Brojot e " + i +" momentalno");
+// 	console.log(i);
+// }
+
+
+// var avtomobili = ""
+// var lista = ["BMW", "AUDI", "VW"]
+// var len = lista.length
+// var i = 0;
+// for(;i<len;){
+// 	avtomobili += lista[i] + " ";
+// 	i++;
+// }
+
+
+// var i;
+// var avtomobili = ""
+// var lista = ["BMW", "AUDI", "VW"]
+// for (x of lista){
+// 	avtomobili += x + " ";
+// 	// console.log(x)
+// 	console.log(avtomobili)
+// }
+
+// avtomobili = ""
+//  for(i=0;i<len;i++){
+//  	if(i==1 || i==3 || i==5){
+//  		continue;
+//  	}
+// 	avtomobili += lista[i] + " ";
+// 	console.log(avtomobili)
+// }
+// console.log(avtomobili)
+
+
+// var avtomobili = "";
+// var lista=["BMW", "AUDI", "VW", "Renault", "Citroen", "Seat", "Peugeot", "Mercedes", "Mitsubishi", "Ford"]
+// 	for(x of lista){
+// 		avtomobili += x + "";
+// 		console.log(x)
+// }
+// console.log(lista)
+
+
+// var avtomobili = "";
+// var lista=["BMW", "AUDI", "VW", "Renault", "Citroen", "Seat", "Peugeot", "Mercedes", "Mitsubishi", "Ford"]
+// var len = lista.length
+// 	for(let i=0; i<len; i++){
+// 		console.log(lista[i]);
+// 		avtomobili+=lista[i] + " ";
+// }
+// console.log(avtomobili)
+// var avtomobili = "";
+
+// for(x of lista){
+// 	console.log(x)
+// }
+
+
+
+// var text = "";
+// var i = 0;
+
+// do{
+// 	text += "Brojot e " + i + " ";
+// 	i++
+// 	console.log(text)
+// }
+// while(i>5);
+// console.log(text);
+
+
+// var text = "";
+// var i = 6;
+// while (i>5){
+// 	text += "Brojot e " + i + " ";
+// 	i++
+// 	console.log(text);
+// 	continue;
+// }
+
+
+
+// var avtomobili = "";
+// var lista=["BMW", "AUDI", "VW", "Renault", "Citroen", "Seat", "Peugeot", "Mercedes", "Mitsubishi", "Ford"]
+// var len = lista.length
+// 	for(let i=0; i<len; i++){
+// 		if (i%2 == 0)
+// 		console.log(lista[i]);
+		
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
