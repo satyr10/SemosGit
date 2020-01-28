ee6cea021573e078a6825de6b3774a7bf883f83c
Adrian Tochi <58231214+satyr10@users.noreply.github.com>
2020-01-28 21:46:09 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Bye} from './Bye';
+import {Hello} from './Hello';
+
+class App extends React.Component{
+
+
+  render(){
+    return(
+      <div>
+        <Hello/>
+        <Bye/>
+      <h2>Hello</h2>
+      
+      </div>
+    )
+  }
+}
+export default App;
---
+++ Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Hello extends React.Component {
+
+    render(){
+        return(
+            <h2>Hello is it me Your looking for</h2>
+        )
+    }
+
+}
